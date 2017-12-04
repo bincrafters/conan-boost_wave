1097de2fe2bf0a11e59ac6047e78aaab9765940f
Rene Rivera <grafikrobot@gmail.com>
2017-12-03 23:37:31 -0600

    Next generation of Boost packages.

--- conanfile.py
+++ conanfile.py
@@ -1,57 +1,57 @@
 from conans import ConanFile, tools
-import os
+
 
 class BoostWaveConan(ConanFile):
     name = "Boost.Wave"
     version = "1.65.1"
-    generators = "boost" 
-    settings = "os", "arch", "compiler", "build_type"
-    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-wave"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_names = ["wave"]
+
+    settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
+
+    requires = \
+        "Boost.Assert/1.65.1@bincrafters/testing", \
+        "Boost.Concept_Check/1.65.1@bincrafters/testing", \
+        "Boost.Config/1.65.1@bincrafters/testing", \
+        "Boost.Core/1.65.1@bincrafters/testing", \
+        "Boost.Date_Time/1.65.1@bincrafters/testing", \
+        "Boost.Filesystem/1.65.1@bincrafters/testing", \
+        "Boost.Iterator/1.65.1@bincrafters/testing", \
+        "Boost.Mpl/1.65.1@bincrafters/testing", \
+        "Boost.Multi_Index/1.65.1@bincrafters/testing", \
+        "Boost.Pool/1.65.1@bincrafters/testing", \
+        "Boost.Preprocessor/1.65.1@bincrafters/testing", \
+        "Boost.Serialization/1.65.1@bincrafters/testing", \
+        "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+        "Boost.Spirit/1.65.1@bincrafters/testing", \
+        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
+        "Boost.Type_Traits/1.65.1@bincrafters/testing"
+
+    lib_short_names = ["wave"]
+    is_header_only = False
+
+    # BEGIN
+
+    short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
-    requires =  "Boost.Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Concept_Check/1.65.1@bincrafters/testing", \
-                      "Boost.Config/1.65.1@bincrafters/testing", \
-                      "Boost.Core/1.65.1@bincrafters/testing", \
-                      "Boost.Date_Time/1.65.1@bincrafters/testing", \
-                      "Boost.Filesystem/1.65.1@bincrafters/testing", \
-                      "Boost.Iterator/1.65.1@bincrafters/testing", \
-                      "Boost.Mpl/1.65.1@bincrafters/testing", \
-                      "Boost.Multi_Index/1.65.1@bincrafters/testing", \
-                      "Boost.Pool/1.65.1@bincrafters/testing", \
-                      "Boost.Preprocessor/1.65.1@bincrafters/testing", \
-                      "Boost.Serialization/1.65.1@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
-                      "Boost.Spirit/1.65.1@bincrafters/testing", \
-                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-                      "Boost.Type_Traits/1.65.1@bincrafters/testing"
+    generators = "boost" 
 
-                      #assert1 concept_check5 config0 core2 filesystem8 iterator5 mpl5 multi_index12 pool11 preprocessor0 serialization11 smart_ptr4 spirit11 static_assert1 throw_exception2 type_traits3
-                      
-    def source(self):
-        boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version  
-        for lib_short_name in self.lib_short_names:
-            tools.get("{0}/{1}/archive/{2}.tar.gz"
-                .format(boostorg_github, lib_short_name, archive_name))
-            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
+    def package_id(self):
+        if self.is_header_only:
+            self.info.header_only()
 
-    def build(self):
-        self.run(self.deps_user_info['Boost.Generator'].b2_command)
+    @property
+    def env(self):
+        try:
+            with tools.pythonpath(super(self.__class__, self)):
+                import boostgenerator # pylint: disable=F0401
+                boostgenerator.BoostConanFile(self)
+        except:
+            pass
+        return super(self.__class__, self).env
 
-    def package(self):
-        self.copy(pattern="*", dst="lib", src="stage/lib")
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)
-
-    def package_info(self):
-        self.user_info.lib_short_names = ",".join(self.lib_short_names)
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.defines.append("BOOST_ALL_NO_LIB=1")
+    # END
