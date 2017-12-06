1df2b5e93f63048accd313b71c0c250c90488baa
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:50 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,12 @@ from conans import ConanFile, tools
 class BoostWaveConan(ConanFile):
     name = "Boost.Wave"
     version = "1.65.1"
-    url = "https://github.com/bincrafters/conan-boost-wave"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
-    license = "www.boost.org/users/license.html"
 
-    settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
 
     requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
         "Boost.Assert/1.65.1@bincrafters/testing", \
         "Boost.Concept_Check/1.65.1@bincrafters/testing", \
         "Boost.Config/1.65.1@bincrafters/testing", \
@@ -36,19 +33,19 @@ class BoostWaveConan(ConanFile):
 
     # BEGIN
 
+    url = "https://github.com/bincrafters/conan-boost-wave"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
-    generators = "boost" 
-
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
+    generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
 
     @property
     def env(self):
         try:
             with tools.pythonpath(super(self.__class__, self)):
-                import boostgenerator # pylint: disable=F0401
+                import boostgenerator  # pylint: disable=F0401
                 boostgenerator.BoostConanFile(self)
         except:
             pass
