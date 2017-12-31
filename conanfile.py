59135e7314461a5d792646eb00c012566bd31607
Jerry W <jerrywiltse@gmail.com>
2017-12-31 18:18:44 -0500

    Update url field in conanfile [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, tools
 class BoostWaveConan(ConanFile):
     name = "boost_wave"
     version = "1.66.0"
-    url = "https://github.com/bincrafters/conan-boost-wave"
+    url = "https://github.com/bincrafters/conan-boost_wave"
 
     lib_short_names = ["wave"]
     is_header_only = False
