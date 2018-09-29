96057644636c046f44f03368ecfb0d33b1384e32
Jerry W <jerrywiltse@gmail.com>
2018-09-29 17:15:46 -0400

    move to python_requires strategy

--- conanfile.py
+++ conanfile.py
@@ -1,82 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import python_requires
 
 
-class BoostWaveConan(ConanFile):
+base = python_requires("boost_base/1.67.0@bincrafters/testing")
+
+class BoostWaveConan(base.BoostBaseConan):
     name = "boost_wave"
-    version = "1.67.0"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    exports = ["LICENSE.md"]
+    url = "https://github.com/bincrafters/conan-boost_wave"
     lib_short_names = ["wave"]
-    is_header_only = False
-
     options = {"shared": [True, False]}
     default_options = "shared=False"
+    b2_requires = [
+        "boost_assert",
+        "boost_concept_check",
+        "boost_config",
+        "boost_core",
+        "boost_filesystem",
+        "boost_iterator",
+        "boost_lexical_cast",
+        "boost_mpl",
+        "boost_multi_index",
+        "boost_pool",
+        "boost_preprocessor",
+        "boost_serialization",
+        "boost_smart_ptr",
+        "boost_spirit",
+        "boost_static_assert",
+        "boost_throw_exception",
+        "boost_type_traits"
+    ]
 
-    requires = (
-        "boost_assert/1.67.0@bincrafters/testing",
-        "boost_concept_check/1.67.0@bincrafters/testing",
-        "boost_config/1.67.0@bincrafters/testing",
-        "boost_core/1.67.0@bincrafters/testing",
-        "boost_filesystem/1.67.0@bincrafters/testing",
-        "boost_iterator/1.67.0@bincrafters/testing",
-        "boost_lexical_cast/1.67.0@bincrafters/testing",
-        "boost_mpl/1.67.0@bincrafters/testing",
-        "boost_multi_index/1.67.0@bincrafters/testing",
-        "boost_package_tools/1.67.0@bincrafters/testing",
-        "boost_pool/1.67.0@bincrafters/testing",
-        "boost_preprocessor/1.67.0@bincrafters/testing",
-        "boost_serialization/1.67.0@bincrafters/testing",
-        "boost_smart_ptr/1.67.0@bincrafters/testing",
-        "boost_spirit/1.67.0@bincrafters/testing",
-        "boost_static_assert/1.67.0@bincrafters/testing",
-        "boost_throw_exception/1.67.0@bincrafters/testing",
-        "boost_type_traits/1.67.0@bincrafters/testing"
-    )
 
-    def package_id_additional(self):
-        boost_deps_only = [dep_name for dep_name in self.info.requires.pkg_names if dep_name.startswith("boost_")]
-
-        for dep_name in boost_deps_only:
-            self.info.requires[dep_name].full_version_mode()
-
-    # BEGIN
-
-    url = "https://github.com/bincrafters/conan-boost_wave"
-    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
-    license = "BSL-1.0"
-    short_paths = True
-    generators = "boost"
-    settings = "os", "arch", "compiler", "build_type"
-    build_requires = "boost_generator/1.67.0@bincrafters/testing"
-
-    def package_id(self):
-        getattr(self, "package_id_additional", lambda:None)()
-
-    def source(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.source(self)
-        getattr(self, "source_additional", lambda:None)()
-
-    def build(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.build(self)
-        getattr(self, "build_additional", lambda:None)()
-
-    def package(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package(self)
-        getattr(self, "package_additional", lambda:None)()
-
-    def package_info(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package_info(self)
-        getattr(self, "package_info_additional", lambda:None)()
-
-    # END
